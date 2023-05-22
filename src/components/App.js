fb1795895295c994f2d6653497a2a17e781ec9ab
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-22 19:51:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,26 @@ import Navigation from "./Navigation";
 import Posts from "./Posts";
 const App = () => {
   const [isLogged, setIsLogged] = useState(false);
+  const [posts, setPosts] = useState([
+    {
+      postedBy: "Kunal",
+      postText:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      upVotes: 3,
+      downVotes: 1
+    },
+    {
+      postedBy: "Kunal",
+      postText:
+        "Lorem Ipsum is simply dummy text of the printing and zxzjjy asdhbb asdbjhbajhbkubas khbkubasx typesetting industrydscbsjdbjsbcsdcbsdjcbkjbsdkj cnlskdcnlksdcnsdklnm.",
+      upVotes: 3,
+      downVotes: 1
+    }
+  ]);
   return (
     <div id="main">
       <Navigation log={isLogged} set={setIsLogged} />
-      <Posts />
+      <Posts data={posts} set={setPosts} />
     </div>
   );
 };
