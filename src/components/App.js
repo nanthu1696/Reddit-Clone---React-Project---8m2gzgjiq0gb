37ebce8bc1721fd12ef91c0bf4cb70775c5839bb
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-22 18:30:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "../styles/App.css";
 import Navigation from "./Navigation";
+import Posts from "./Posts";
 const App = () => {
   const [isLogged, setIsLogged] = useState(false);
   return (
     <div id="main">
       <Navigation log={isLogged} set={setIsLogged} />
+      <Posts />
     </div>
   );
 };
