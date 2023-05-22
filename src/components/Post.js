5d295de58d29834c7a954518ac5de2d75cc4752e
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-22 19:50:05 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "boxicons";
 
-const Post = () => {
-  return <div></div>;
+const Post = (props) => {
+  const { data } = props;
+  return (
+    <div className="singlepost">
+      <div className="voteContainer">
+        <div className="buttonContainer">
+          <button>
+            <box-icon type="solid" name="up-arrow"></box-icon>
+          </button>
+          <span>{data.upVotes}</span>
+        </div>
+        <div className="buttonContainer">
+          <button>
+            <box-icon type="solid" name="down-arrow"></box-icon>
+          </button>
+          <span>{data.downVotes}</span>
+        </div>
+      </div>
+      <div className="postText">{data.postText}</div>
+    </div>
+  );
 };
 
 export default Post;
