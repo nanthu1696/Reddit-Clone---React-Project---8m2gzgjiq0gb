9e7ee51738c93ba05c02581b338ec4591ab0b2a4
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-22 18:30:58 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,11 +4,12 @@ const Navigation = (props) => {
   const { log, set } = props;
   return (
     <div id="navigationbar">
+      <h1>Reddit</h1>
       {log ? (
-        <>
+        <div>
           <button>Add new post</button>
           <button onClick={() => set(false)}>Log out</button>
-        </>
+        </div>
       ) : (
         <button onClick={() => set(true)}>Log in</button>
       )}
