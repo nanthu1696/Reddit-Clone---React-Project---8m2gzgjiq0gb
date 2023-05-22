ea176ef76348fe4c733339d75c35d061495c9ab3
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-22 18:31:30 +0530

    Create Posts.js

---
+++ src/components/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./Post";
+const Posts = () => {
+  return (
+    <div id="outContainer">
+      <div id="inContainer">
+        <Post />
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
