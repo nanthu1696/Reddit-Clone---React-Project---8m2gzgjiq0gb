0d1109604c60a4466520d723d310826181634e52
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-23 17:28:42 +0530

    Update Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Post from "./Post";
 const Posts = (props) => {
-  const { data, set } = props;
+  const { data, set, log, setPage } = props;
   return (
     <div id="outContainer">
       <div id="inContainer">
         {data.map((post, index) => (
-          <Post data={post} />
+          <Post
+            key={index}
+            index={index}
+            data={post}
+            log={log}
+            setPage={setPage}
+            set={set}
+          />
         ))}
       </div>
     </div>
