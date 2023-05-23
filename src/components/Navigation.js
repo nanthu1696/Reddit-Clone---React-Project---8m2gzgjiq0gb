fdb52e2f77e31a09268bd610ff9359209fae1870
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-23 14:49:45 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Login from "./Login";
+import NewPost from "./NewPost";
 
 const Navigation = (props) => {
   const { log, set } = props;
+  const [page, setPage] = useState(false);
+  const [newPost, setNewPost] = useState(false);
   return (
-    <div id="navigationbar">
-      <h1>Reddit</h1>
-      {log ? (
-        <div>
-          <button>Add new post</button>
-          <button onClick={() => set(false)}>Log out</button>
-        </div>
-      ) : (
-        <button onClick={() => set(true)}>Log in</button>
-      )}
-    </div>
+    <>
+      <div id="navigationbar">
+        <h1>Reddit</h1>
+        {log ? (
+          <div>
+            <button onClick={() => setNewPost(true)}>Add new post</button>
+            <button onClick={() => set(false)}>Log out</button>
+          </div>
+        ) : (
+          <button onClick={() => setPage(true)}>Log in</button>
+        )}
+      </div>
+      {page && <Login set={setPage} setlog={set} />}
+      {newPost && <NewPost set={setNewPost} />}
+    </>
   );
 };
 
