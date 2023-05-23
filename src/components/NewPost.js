0d4974850d7b2d24eb54c9c9c1f294bac465924f
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-23 17:27:59 +0530

    Update NewPost.js

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewPost = (props) => {
-  const { set } = props;
-  function addPost() {}
+  const { set, setpost, user, data } = props;
+  const [title, setTitle] = useState("");
+  const [description, setdescrip] = useState("");
+  function addPost() {
+    let obj = {
+      postedBy: user,
+      title: title,
+      postText: description,
+      upVotes: 0,
+      downVotes: 0
+    };
+    setpost([...data, obj]);
+    set(false);
+  }
   return (
     <div className="popup">
       <div id="newpostcontainer">
@@ -12,12 +24,22 @@ const NewPost = (props) => {
         <form id="postform" action="#">
           <h2>Add new post:</h2>
           <div className="inputbox">
-            <input type="text" required />
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
             <label for="title">Title</label>
           </div>
           <div className="textbox">
             <label for="description">Description:</label>
-            <textarea id="myTextArea" required></textarea>
+            <textarea
+              id="myTextArea"
+              value={description}
+              onChange={(e) => setdescrip(e.target.value)}
+              required
+            ></textarea>
           </div>
           <div id="buttonbox">
             <button onClick={() => set(false)}>Close</button>
