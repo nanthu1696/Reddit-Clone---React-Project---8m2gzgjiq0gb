b27612ab5fd344b069fa379110cd1898e563f8e3
Nanthakumar Sathiyanandham <118247818+nanthu1696@users.noreply.github.com>
2023-05-23 17:28:19 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import "boxicons";
 
 const Post = (props) => {
-  const { data } = props;
+  let { data, log, setPage, set, index } = props;
+  const [clicks, setClicks] = useState({
+    no: 0,
+    by: ""
+  });
+  function upVote() {
+    if (log) {
+      if (clicks.no === 0) {
+        setClicks({ by: "upvote", no: 1 });
+        set((prevArray) => {
+          const newArray = [...prevArray];
+          newArray[index] = {
+            ...newArray[index],
+            upVotes: newArray[index].upVotes + 1,
+            downVotes: newArray[index].downVotes - 1
+          };
+          return newArray;
+        });
+      } else if (clicks.no === 1) {
+        if (clicks.by === "downvote") {
+          setClicks({ by: "upvote", no: 1 });
+          set((prevArray) => {
+            const newArray = [...prevArray];
+            newArray[index] = {
+              ...newArray[index],
+              upVotes: newArray[index].upVotes + 1,
+              downVotes: newArray[index].downVotes - 1
+            };
+            return newArray;
+          });
+        }
+      }
+    } else {
+      setPage(true);
+    }
+  }
+  function downVote() {
+    if (log) {
+      if (clicks.no === 0) {
+        setClicks({ by: "downvote", no: 1 });
+        set((prevArray) => {
+          const newArray = [...prevArray];
+          newArray[index] = {
+            ...newArray[index],
+            downVotes: newArray[index].downVotes + 1,
+            upVotes: newArray[index].upVotes - 1
+          };
+          return newArray;
+        });
+      } else if (clicks.no === 1) {
+        if (clicks.by === "upvote") {
+          setClicks({ by: "downvote", no: 1 });
+          set((prevArray) => {
+            const newArray = [...prevArray];
+            newArray[index] = {
+              ...newArray[index],
+              downVotes: newArray[index].downVotes + 1,
+              upVotes: newArray[index].upVotes - 1
+            };
+            return newArray;
+          });
+        }
+      }
+    } else {
+      setPage(true);
+    }
+  }
   return (
     <div className="singlepost">
       <div className="voteContainer">
         <div className="buttonContainer">
-          <button>
+          <button onClick={upVote}>
             <box-icon type="solid" name="up-arrow"></box-icon>
           </button>
           <span>{data.upVotes}</span>
         </div>
         <div className="buttonContainer">
-          <button>
+          <button onClick={downVote}>
             <box-icon type="solid" name="down-arrow"></box-icon>
           </button>
           <span>{data.downVotes}</span>
         </div>
       </div>
-      <div className="postText">{data.postText}</div>
+      <div className="postText">
+        <h3>{data.title}</h3>
+        <p>{data.postText}</p>
+      </div>
     </div>
   );
 };
